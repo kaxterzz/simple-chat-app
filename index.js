6d565ba58c3797c7a667f9f4d33ecec8f8364f05
Thilanka Kasun <kasunsmbox@hotmail.com>
2018-06-27 11:01:54 +0530

    Inital commit

---
+++ index.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  var ip = req.headers['x-forwarded-for'] ||
+       req.connection.remoteAddress ||
+       req.socket.remoteAddress ||
+       (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log(ip);
+    next();
+});
+
+var numUsers = 0;
+
+io.on('connection', function(socket){
+var addedUser = false;
+
+socket.on('add user', function(data){
+  var colorName = randomColorGen();
+  socket.color = colorName;
+  socket.username = data;
+  ++numUsers;
+  addedUser = true;
+  socket.emit('greetings msg', {
+    colorName : colorName,
+    gmsg : 'Welcome to Simple Chat',
+    numUsers : numUsers
+  });
+  socket.broadcast.emit('online users',{
+    colorName: colorName,
+    username : socket.username,
+    numUsers : numUsers
+  });
+  socket.broadcast.emit('add user', {
+    colorName: colorName,
+    username : socket.username,
+    numUsers : numUsers
+  });
+});
+
+socket.on('new message', function(data){
+  socket.emit('my message', {
+    colorName: socket.color,
+    username: 'You',
+    message: data
+  });
+  socket.broadcast.emit('new message', {
+    colorName: socket.color,
+    username: socket.username,
+    message: data
+  });
+
+});
+
+socket.on('is typing', function(data){
+  if(data == true){
+    socket.broadcast.emit('is typing', {
+      colorName: socket.color,
+      username: socket.username,
+      message: 'typing'
+    });
+  }else{
+    socket.broadcast.emit('is typing', {
+      username: '',
+      message: ''
+    });
+  }
+});
+
+socket.on('disconnect', function(data){
+  if(addedUser){
+    --numUsers;
+    socket.broadcast.emit('user left', {
+      colorName: socket.color,
+      username: socket.username,
+      numUsers : numUsers
+    });
+  }
+});
+
+});
+
+function randomColorGen(){
+  var randomColor = '#' + ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+  return randomColor;
+}
+
+http.listen(process.env.PORT || 3000, function(){
+ console.log('listening on *:',process.env.PORT);
+});
---
+++ public/main.js
@@ -0,0 +1,109 @@
+$('#nickNameModal').modal({
+   backdrop: 'static',
+   keyboard: false
+ });
+$(document).ready(function(){
+  $('#nickname-btn').attr('disabled','disabled');
+  $('#nick-name').keyup(function(){
+    if($(this).val().length !=0){
+        $('#nickname-btn').attr('disabled', false);
+    }else{
+        $('#nickname-btn').attr('disabled',true);
+    }
+  });
+});
+
+$(function() {
+    var typing = false;
+    var socket = io();
+    var timeout;
+
+    $('#nickname-form').submit(function() {
+        $('#nickNameModal').modal('hide');
+        socket.emit('add user', $('#nick-name').val());
+        $('#nick-name').val('');
+        return false;
+    });
+
+    socket.on('greetings msg', function(data){
+      $('#info-msg').append($('<span>').addClass('greetings-msg').css({'color':data.colorName}).text(data.gmsg)).append($('</span><br>'));
+      $('#num-of-online').css({'color':data.colorName}).text('Total online : ' + data.numUsers);
+    });
+
+    socket.on('add user', function(data) {
+        $('#info-msg').append($('<span>').addClass('joined-uname').css({'color':data.colorName}).text(data.username + ' joined')).append($('</span><br>'));
+        $('#num-of-online').css({'color':data.colorName}).text('Total online : ' + data.numUsers);
+    });
+
+    $('#m').keypress(function(e) {
+        if (e.keyCode == 13) {
+            socket.emit('new message', $('#m').val());
+            $('#m').val('');
+            return false;
+        } else {
+            typing = true;
+            socket.emit('is typing', typing);
+            clearTimeout(timeout);
+            timeout = setTimeout(timeoutFunction, 2000);
+        }
+
+    });
+
+    socket.on('is typing', function(data){
+      if(data.username != ''){
+        $('#username-of-person').addClass('typing-username').css({'color':data.colorName}).text(data.username + ' : ');
+        $('#user-typing').text(data.message);
+      }else{
+        $('#username-of-person').html('');
+        $('#user-typing').html('');
+      }
+    });
+
+    socket.on('my message', function(data){
+      $('<div>',{
+          'class' : 'msg-wrapper alert alert-secondary',
+          'role' : 'alert',
+          'html': $('<span>',{
+            'class' : 'inner participant-name',
+            'style' : 'color:'+data.colorName
+          }).text(data.username+' : ').add($('<span>',{
+            'class' : 'inner participant-msg'
+          }).text(data.message))
+        }).appendTo('#messages');
+    });
+    socket.on('new message', function(data) {
+      $('<div>',{
+          'class' : 'msg-wrapper alert alert-secondary',
+          'role' : 'alert',
+          'html': $('<span>',{
+            'class' : 'inner participant-name',
+            'style' : 'color:'+data.colorName
+          }).text(data.username+' : ').add($('<span>',{
+            'class' : 'inner participant-msg'
+          }).text(data.message))
+        }).appendTo('#messages');
+    });
+
+    socket.on('online users', function(data){
+      $('#num-of-online').css({'color':data.colorName}).text('Total online : ' + data.numUsers);
+      $('#online-users').append($('<li>').addClass('online-users-list').css({'color':data.colorName}).text(data.username)).append($('</li>'));
+    });
+
+    socket.on('user left', function(data){
+      $('#info-msg').append($('<span>').addClass('left-uname').css({'color':data.colorName}).text(data.username + ' left')).append($('</span><br>'));
+      $('#num-of-online').css({'color':data.colorName}).text('Total online : ' + data.numUsers);
+      removeOnlinename(data.username);
+      // if(onlinestatus != 0){
+      //   $(this).remove();
+      // }
+    });
+
+    function removeOnlinename(username){
+      $('#online-users li').filter(function() { return $.text([this]) === username; }).remove();
+    }
+    function timeoutFunction(){
+      typing = false;
+      socket.emit('is typing', typing);
+    }
+
+});
